f520672079afae31155b51a23a38ad4dbc5792bc
Teerasej <teerasej@gmail.com>
2019-07-25 11:58:04 +0700

    เพิ่มฟังก์ชั่นหาร

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 module.exports = {
     plus: (first, second) => first + second,
     subtract: (first, second) => first - second,
-    multiply: (first, second) => first * second
-    // ทดสอบ
+    multiply: (first, second) => first * second,
+    divide: (first, second) => first / second
 }
